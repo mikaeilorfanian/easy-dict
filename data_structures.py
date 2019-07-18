55105abe1dbcdc645525bc8cee2c06b41dbdace9
Mikaeil Orfanian <mokt@outlook.com>
2019-07-18 10:20:20 +0200

    first commit

---
+++ data_structures.py
@@ -0,0 +1,59 @@
+from copy import deepcopy
+
+
+class EasyDictError(Exception):
+    """Custom exception for working with EasyDict instances"""
+
+
+class EasyAccessDict:
+    def __init__(self, raw_data):
+        self.raw_data = deepcopy(raw_data)
+
+        if isinstance(self.raw_data, list):
+            self.clean_data = [EasyAccessDict(v) if isinstance(v, dict) else v for v in self.raw_data]
+        else:
+            self.clean_data = self.raw_data
+
+    def __getattr__(self, attribute_name):
+        try:
+            if attribute_name == 'first':
+                if isinstance(self.clean_data, list):
+                    return self.clean_data[0]
+
+            value = self.clean_data[attribute_name]
+
+            if isinstance(value, (dict, list)):
+                return EasyAccessDict(value)
+
+            return value
+
+        except KeyError:
+            raise EasyDictError('No attr/key called "{}"'.format(attribute_name))
+
+    def get(self, key, default=None):
+        try:
+            return self.clean_data[key]
+        except KeyError:
+            return default
+
+    @property
+    def json(self):
+        return self.raw_data
+
+    def __getitem__(self, index):
+        if isinstance(self.clean_data, list):
+            return self.raw_data[index]
+        else:
+            raise ValueError('EasyDict __getitem__ "{}" failed!'.format(index))
+
+    def __eq__(self, other):
+        return self.raw_data == other.raw_data
+
+    def __iter__(self):
+        return iter(self.clean_data)
+
+    def __str__(self):
+        return str(self.raw_data)
+
+    def __repr__(self):
+        return str(self.raw_data)
---
+++ test_data_structures.py
@@ -0,0 +1,42 @@
+import pytest
+
+from data_structures import EasyAccessDict, EasyDictError
+
+
+def test_easy_dict():
+    regular_dict = {'a': 1}
+    easy_dict = EasyAccessDict(regular_dict)
+    assert regular_dict['a'] == easy_dict.a
+    with pytest.raises(EasyDictError) as e:
+        easy_dict.b
+        assert '"b"' in e
+
+    regular_dict = {'a': 1, 'b': {'c': 2}}
+    easy_dict = EasyAccessDict(regular_dict)
+    assert regular_dict['a'] == easy_dict.a
+    assert regular_dict['b']['c'] == easy_dict.b.c
+    with pytest.raises(EasyDictError) as e:
+        easy_dict.b.d
+        assert '"d"' in e
+    with pytest.raises(AttributeError):
+        easy_dict.b.c.d
+
+    regular_dict = {'a': 1, 'b': {'e': 1, 'c': [{'d': 2}]}}
+    easy_dict = EasyAccessDict(regular_dict)
+    assert regular_dict['a'] == easy_dict.a
+    assert regular_dict['b']['e'] == easy_dict.b.e
+    with pytest.raises(EasyDictError) as e:
+        easy_dict.b.d
+        assert '"d"' in e
+    with pytest.raises(TypeError):
+        easy_dict.b.c.d
+    assert regular_dict['b']['c'][0]['d'] == easy_dict.b.c.first.d
+    with pytest.raises(AttributeError):
+        easy_dict.a.first
+    with pytest.raises(EasyDictError):
+        easy_dict.b.first
+        assert '"first"' in e
+
+    regular_dict = {1: '1'}
+    easy_dict = EasyAccessDict(regular_dict)
+    assert regular_dict[1] == easy_dict.get(1)
